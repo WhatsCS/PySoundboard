d74e31de159dc46ac81a20451d2c148202ac20df
WhatsCS <whatscs@jointheb.org>
2019-05-05 15:26:12 -0700

    all in commit for desktop

--- soundboard.py
+++ soundboard.py
@@ -8,7 +8,7 @@ import queue
 import sounddevice as sd
 import threading
 import yaml
-from lib.audio import Audio
+from lib.audio import play_file, stop_audio
 from lib.utils import get_devs
 from pprint import pprint
 
@@ -22,33 +22,33 @@ def int_or_str(text):
 
 
 parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument(
-    '-c',
-    '--config',
-    help='load config from non-standard location',
-    default='None')
-parser.add_argument(
-    '-d',
-    '--device',
-    type=int_or_str,
-    help='output device (numeric ID or substring)')
-parser.add_argument(
-    '-b',
-    '--blocksize',
-    type=int,
-    default=2048,
-    help='block size (default: %(default)s)')
+parser.add_argument('-c',
+                    '--config',
+                    help='load config from non-standard location',
+                    default='None')
+parser.add_argument('-d',
+                    '--device',
+                    type=int_or_str,
+                    help='output device (numeric ID or substring)')
+parser.add_argument('-b',
+                    '--blocksize',
+                    type=int,
+                    default=2048,
+                    help='block size (default: %(default)s)')
 parser.add_argument(
     '-q',
     '--buffersize',
     type=int,
     default=20,
     help='number of blocks used for buffering (default: %(default)s)')
-parser.add_argument(
-    '-ld',
-    '--list-devices',
-    action='store_true',
-    help='get list of available devices')
+parser.add_argument('-ld',
+                    '--list-devices',
+                    action='store_true',
+                    help='get list of available devices')
+# parser.add_argument('--stop-playback',
+#                     action='store_true',
+#                     default=False,
+#                     help='Stops all audio playback')
 # TODO: Add in --gui argument
 args = parser.parse_args()
 if args.blocksize == 0:
@@ -57,10 +57,9 @@ if args.buffersize < 1:
     parser.error('buffersize must be at least 1')
 if args.list_devices is True:
     get_devs()
-
-# Create thread pool
-q = queue.Queue(maxsize=args.buffersize)
-event = threading.Event()
+# if args.stop_playback is True:
+#     stop_audio()
+#     parser.exit(0, 'Audio Stopped!')
 
 # Load config into args (for now)
 with open('config.yaml') as conf:
@@ -73,11 +72,8 @@ config.update(vars(args))
 #TODO: Figure out how to get information of the currently running stream
 #      I'll probably have to figure out threading more properly cause my head hurts
 try:
-    Audio(args=config, queue=q, event=event).play_file()
+    play_file(args=config)
 except KeyboardInterrupt:
     parser.exit('\nInterrupted by user')
-except queue.Full:
-    # A timeout occured, i.e. there was an error in the callback
-    parser.exit(1)
 except Exception as e:
     parser.exit(type(e).__name__ + ': ' + str(e))
